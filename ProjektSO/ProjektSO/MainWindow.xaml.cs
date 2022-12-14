f50e750c6056965bfbeaf25c1b31f9eca8ac306f
dciepko <dciepko17@gmail.com>
2022-12-14 21:04:42 +0100

    14.12

--- ProjektSO/ProjektSO/MainWindow.xaml.cs
+++ ProjektSO/ProjektSO/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Media.Animation;
 
 namespace ProjektSO
 {
@@ -23,10 +25,50 @@ namespace ProjektSO
         public MainWindow()
         {
             InitializeComponent();
-            
-            
+
+            //Thread car1 = new Thread(Car1);
+            //car1.Start();
+
+            //    Rectangle autko1 = new Rectangle();
+            //    autko1.Width = 40;
+            //    autko1.Height = 20;
+            //    autko1.Fill = Brushes.Blue;
+
+            //    MainCanvas.Children.Add(autko1);
+            //    Canvas.SetTop(autko1, 140);
+
+            //    RectAnimation animation1 = new RectAnimation();
+
+            //    Storyboard sb1 = new Storyboard();
+
+
+
+            //    sb1.Children.Add(animation1);
+            //}
+
+            //public class RectAnimation : AnimationTimeline
+            //{
+            //    public override Type TargetPropertyType => throw new NotImplementedException();
+
+            //    protected override Freezable CreateInstanceCore()
+            //    {
+            //        throw new NotImplementedException();
+            //    }
+            //}
+
+            //void Car1()
+            //{
+            //    Rectangle autko1 = new Rectangle();
+            //    autko1.Width = 40;
+            //    autko1.Height = 20;
+            //    autko1.Fill = Brushes.Red;
+
+            //    MainCanvas.Children.Add(autko1);
+            //    Canvas.SetTop(autko1, 140);
+            //}
+
+
         }
 
-        
     }
 }
