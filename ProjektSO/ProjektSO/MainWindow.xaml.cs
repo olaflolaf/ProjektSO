fed316fa927c2126ddfda15b9aa92849341b6ece
dciepko <dciepko17@gmail.com>
2022-12-21 19:50:38 +0100

    21.12

--- ProjektSO/ProjektSO/MainWindow.xaml.cs
+++ ProjektSO/ProjektSO/MainWindow.xaml.cs
@@ -22,46 +22,59 @@ namespace ProjektSO
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Rectangle> rectangles = new List<Rectangle>();
+
         public MainWindow()
         {
             InitializeComponent();
 
-            //Rectangle autko1 = new Rectangle();
-            //autko1.Width = 40;
-            //autko1.Height = 20;
-            //autko1.Fill = Brushes.Blue;
+            Rectangle autko = new Rectangle();
+            autko.Width = 40;
+            autko.Height = 20;
+            autko.Fill = Brushes.Blue;
+            MainCanvas.Children.Add(autko);
+            Canvas.SetTop(autko, 145);
+            rectangles.Add(autko);
 
-            //Thread car1 = new Thread(Car1);
-            //car1.Start(autko1);
+            Rectangle ciufa = new Rectangle();
+            ciufa.Width = 20;
+            ciufa.Height = 60;
+            ciufa.Fill = Brushes.Red;
+            MainCanvas.Children.Add(ciufa);
+            Canvas.SetBottom(ciufa, 0);
+            Canvas.SetRight(ciufa, 264);
+            rectangles.Add(ciufa);
 
+            Thread car1 = new Thread(Car1);
+            car1.Start();
 
-
-            //MainCanvas.Children.Add(autko1);
-            //Canvas.SetTop(autko1, 140);
+            Thread train = new Thread(Train);
+            train.Start();
 
         }
 
 
+        public void Car1()
+        {
+            for (int i = 0; i < 480; i++)
+            {
+                this.Dispatcher.Invoke(() =>
+                { Canvas.SetLeft(rectangles[0], i); });
+                Thread.Sleep(10);
+            }         
+        }
 
-        //public void Car1(Rectangle autko1)
-        //{
-        //    //Rectangle autko1 = new Rectangle();
-        //    //autko1.Width = 40;
-        //    //autko1.Height = 20;
-        //    //autko1.Fill = Brushes.Red;
-
-        //    MainCanvas.Children.Add(autko1);
-        //    Canvas.SetTop(autko1, 140);
-
-        //    for(int i =0; i<480;i++)
-        //    {
-        //        Canvas.SetLeft(autko1, i);
-        //        Thread.Sleep(100);
-        //    }
-        //}
-
-
+        
+        public void Train()
+        {
+            for (int i = 0; i < 600; i++)
+            {
+                this.Dispatcher.Invoke(() =>
+                { Canvas.SetBottom(rectangles[1], i); });
+                Thread.Sleep(5);
+            }
+        }
     }
-
 }    
 
+//x i y sie przydadza potem (petla while np)
