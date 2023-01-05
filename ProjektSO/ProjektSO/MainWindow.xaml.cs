a4b0c39272f4aa6baf2f81a905a6935552c6fe78
dciepko <dciepko17@gmail.com>
2023-01-05 22:58:06 +0100

    5.01

--- ProjektSO/ProjektSO/MainWindow.xaml.cs
+++ ProjektSO/ProjektSO/MainWindow.xaml.cs
@@ -23,10 +23,15 @@ namespace ProjektSO
     public partial class MainWindow : Window
     {
         List<Rectangle> rectangles = new List<Rectangle>();
-
+        double xa = 0;
+        double ya = 0;
+        double x = 0;
+        double y = 0;
         public MainWindow()
         {
             InitializeComponent();
+
+           
 
             Rectangle autko = new Rectangle();
             autko.Width = 40;
@@ -35,6 +40,7 @@ namespace ProjektSO
             MainCanvas.Children.Add(autko);
             Canvas.SetTop(autko, 145);
             rectangles.Add(autko);
+
 
             Rectangle ciufa = new Rectangle();
             ciufa.Width = 20;
@@ -56,15 +62,126 @@ namespace ProjektSO
 
         public void Car1()
         {
-            for (int i = 0; i < 480; i++)
+            ya = 145;
+
+            for (int i = 0; i < 490; i++)
             {
                 this.Dispatcher.Invoke(() =>
                 { Canvas.SetLeft(rectangles[0], i); });
+                xa++;
                 Thread.Sleep(10);
-            }         
+            }
+
+            
+
+            Thread turn = new Thread(Turn1);
+            turn.Start();
         }
 
+        public void Turn1()
+        {
+            double angle=3.6;
+            double r = 91;
+
+            while(angle>-0.3)
+            {
+                angle = angle-0.1;
+                x = xa + r * Math.Sin(angle);
+                y = ya + r * Math.Cos(angle);
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    Canvas.SetLeft(rectangles[0], x+30);
+                    Canvas.SetTop(rectangles[0], y+91);
+                }); 
+                Thread.Sleep(40);
+            }
+
+            Thread autko2 = new Thread(Car2);
+            autko2.Start();
+
+        }
+
+        public void Car2()
+        {
+            xa = x + 30;
+            ya = y + 91;
+
+            this.Dispatcher.Invoke(() =>
+            { rectangles[0].Fill = Brushes.Green; }
+            );
+
+
+            this.Dispatcher.Invoke(() =>
+            {
+                Canvas.SetLeft(rectangles[0], xa);
+                Canvas.SetTop(rectangles[0], ya);
+            });
+
+            for (double i = xa; i > 160; i--)
+            {
+                
+                this.Dispatcher.Invoke(()=>
+                {
+                    Canvas.SetLeft(rectangles[0], i);
+                });
+                xa--;
+                Thread.Sleep(10);
+            }
+
+            Thread turn2 = new Thread(Turn2);
+            turn2.Start();
+
+        }
         
+
+        public void Turn2()
+        {
+            double angle = 3.6;
+            double r = 62;
+
+            while (angle <6)
+            {
+                angle = angle + 0.1;
+                x = xa + r * Math.Sin(angle);
+                y = ya + r * Math.Cos(angle);
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    Canvas.SetLeft(rectangles[0], x + 15);
+                    Canvas.SetTop(rectangles[0], y + 62);
+                });
+                Thread.Sleep(40);
+            }
+
+            Thread autko3 = new Thread(Car3);
+            autko3.Start();
+
+        }
+
+        public void Car3()
+        {
+            xa = x + 15;
+            ya = y + 62;
+
+            this.Dispatcher.Invoke(() =>
+            {
+                Canvas.SetLeft(rectangles[0], xa);
+                Canvas.SetTop(rectangles[0], ya);
+            });
+
+            for (double i = xa; i < 800; i++)
+            {
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    Canvas.SetLeft(rectangles[0], i);
+                });
+                xa++;
+                Thread.Sleep(10);
+            }
+        }
+  
         public void Train()
         {
             for (int i = 0; i < 600; i++)
